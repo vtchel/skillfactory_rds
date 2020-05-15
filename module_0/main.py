f93464a697541948ac63cf83325ff7aaac03151b
Tchelyubeev, Vladimir <vtchel@yandex.ru>
2020-05-15 17:00:11 +0300

    fix mentor comments

--- module_0/main.py
+++ module_0/main.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from argparse import ArgumentParser
 
-MAX_INT = 101
+
+MAX_INT = 100
 RANDOM_SEED = 1
 
 
@@ -19,11 +21,12 @@ def game_core_v1(number):
 
 
 def game_core_v2(number):
-    """Простой перебор"""
+    """Простой перебор.
+       Функция принимает загаданное число и возвращает число попыток"""
     count = 0
     while count != number:
         count += 1
-    return count
+    return count         # выход из цикла, если угадали
 
 
 def game_core_v3(number):
@@ -40,27 +43,21 @@ def game_core_v3(number):
     return count         # выход из цикла, если угадали
 
 
-def get_median(start, end):
-    res = int((end + start)/2)   # ищется сеередина и округляется в меньшую сторону
-    # print(res)
-    return res
-
-
 def game_core_v4(number):
     """Делим на 2, определяем больше меньше"""
     count = 1
     start = 1
     end = MAX_INT
-    predict = get_median(start, end)    # определеяем середину
+    predict = (start + end)//2          # определеяем середину, округление в меньшую сторону
     while number != predict:
         count += 1
         if number > predict:
             start = predict             # сокращаем интервал, переносим начало
-            predict = get_median(start, end)
+            predict = (start + end)//2
         elif number < predict:
             end = predict               # сокращаем интервал, переносим конец
-            predict = get_median(start, end)
-    return count         # выход из цикла, если угадали
+            predict = (start + end)//2
+    return count                        # выход из цикла, если угадали
 
 
 def score_game(game_core):
@@ -76,4 +73,30 @@ def score_game(game_core):
 
 
 # запускаем
-score_game(game_core_v4)
+if __name__ == '__main__':
+    parser = ArgumentParser('python3 main.py',
+                            description='''Игра Угадай число. Без аргументов запуск интерактивной части.''')
+    parser.add_argument('--bench', dest='bench',
+                        action='store_true', help='Запуск теста алгоритмов поиска')
+    args = parser.parse_args()
+
+    if args.bench:
+        print('Тест game_core_v1: ', end='')
+        score_game(game_core_v1)
+
+        print('Тест game_core_v2: ', end='')
+        score_game(game_core_v2)
+
+        print('Тест game_core_v3: ', end='')
+        score_game(game_core_v3)
+
+        print('Тест game_core_v4: ', end='')
+        score_game(game_core_v4)
+    else:
+        # Запрос числа пользователя в интервале от 1 до 99.
+        number = int(input('Загадайте число от 1 до 99:\n'))
+
+        # Запуск
+        score_count = game_core_v4(number)
+        print(f'Алгоритм угадал число за {score_count} попыток.')
+
